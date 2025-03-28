50ed85975af77f826c16e4541edb219b12794255
Antarip Gohain <antarip.767@gmail.com>
2025-03-29 01:37:50 +0530

    installed cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
+  adapter: cloudflare()
 });
